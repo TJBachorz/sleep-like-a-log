8d500f8e8d4cc2eb4c81a7fe6f51f2d309a4a9e7
TJ <tjbachorz@gmail.com>
2021-09-18 14:00:30 -0600

    creates sleep db with pg

--- data/db.js
+++ data/db.js
@@ -1,1 +1,18 @@
-const knex = require('knex');
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
+const { DB_PASSWORD, DB_HOST, DB_USER, DB_NAME, DB_PORT } = process.env;
+
+const { Pool } = require('pg')
+const pool = new Pool({
+    user: DB_USER,
+    host: DB_HOST,
+    database: DB_NAME,
+    password: DB_PASSWORD,
+    port: DB_PORT,
+})
+pool.query('SELECT NOW()', (err, res) => {
+    console.log(err, res) 
+    pool.end() 
+})
